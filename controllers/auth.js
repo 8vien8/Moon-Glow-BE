bdee5faf2267915364428c1dab07cb43b3553e92
8vien8 <vudinhvien890@gmail.com>
2025-05-12 18:17:43 +0700

    fix: update cookie settings in login function to allow cross-origin requests

--- controllers/auth.js
+++ controllers/auth.js
@@ -34,12 +34,19 @@ const login = async (req, res, next) => {
             { expiresIn: "15m" }
         );
 
-        // Gửi token qua HTTP-only cookie
+        // // Gửi token qua HTTP-only cookie
+        // res.cookie("token", token, {
+        //     httpOnly: true,
+        //     secure: process.env.NODE_ENV === "production", // chỉ dùng HTTPS ở production
+        //     sameSite: "Strict", // hoặc "Lax"
+        //     maxAge: 15 * 60 * 1000 // 15 phút
+        // });
+
         res.cookie("token", token, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === "production", // chỉ dùng HTTPS ở production
-            sameSite: "Strict", // hoặc "Lax"
-            maxAge: 15 * 60 * 1000 // 15 phút
+            secure: true,
+            sameSite: "None", // allow cross-origin frontend to receive cookie
+            maxAge: 15 * 60 * 1000
         });
 
         res.json({

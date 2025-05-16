13c23681c135dc1646b5456576c110c1ea0190f9
8vien8 <vudinhvien890@gmail.com>
2025-05-16 14:39:58 +0700

    fix: remove unused error handling in login function

--- controllers/auth.js
+++ controllers/auth.js
@@ -64,7 +64,6 @@ const login = async (req, res, next) => {
         res.status(200).json({ message: "Login successful" });
     } catch (error) {
         res.status(500).json({ error: "Login failed" });
-        next(error);
     }
 };
 
